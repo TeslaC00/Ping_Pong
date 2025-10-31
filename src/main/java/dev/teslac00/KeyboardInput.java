986d8d72dc34ddb54b43951cca18c1ca1d686d1e
TeslaC00 <111948419+TeslaC00@users.noreply.github.com>
2025-10-31 19:23:12 +0530

    Fix: KeyboardInput does not crash on unknown key

--- src/main/java/dev/teslac00/KeyboardInput.java
+++ src/main/java/dev/teslac00/KeyboardInput.java
@@ -11,6 +11,7 @@ public class KeyboardInput extends GLFWKeyCallback {
 
     @Override
     public void invoke(long window, int key, int scancode, int action, int mods) {
+        if (key < 0 || key > GLFW_KEY_LAST) return;  // guard clause for UNKNOWN_KEY
         keys[key] = action != GLFW_RELEASE;
     }
 }
